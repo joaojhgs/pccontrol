3e571ca6782181639f4acbc92a623a54cb58f281
joaojhgs <joaosperandio@alunos.utfpr.edu.br>
2021-09-24 20:14:14 -0300

    Start script and new commands
    Added a start VBS script for stealth's initialization and new commands.

--- PcControl.bat
+++ PcControl.bat
@@ -16,21 +16,47 @@ rem echo %Executed%
 REM VERIFY IF THE ACTION HAVE ALREADY BEEN EXECUTED, AND IF IT DOESN'T, VERIFIES AND EXECUTE THE ACTION
 REM YOU CAN ADD YOUR CUSTOM ACTIONS HERE, JUST COPY ONE "IF" STRUCTURE, REPLACE THE ACTION WITH THE ONE YOU HAVE SET ON YOUR HTTP REQUEST, AND CONFIGURE WHICH COMMANDS THE CLI SHOULD EXECUTE
 if %Executed%=="false" (
-if %Action% == "restart" (
-curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
-shutdown -s -t 10)
+    if %Action% == "restart" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd exitwin reboot)
 
-if %Action% == "shutdown" (
-curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
-shutdown)
+    if %Action% == "shutdown" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd exitwin shutdown)
 
-if %Action% == "volUp" (
-curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
-nircmd changesysvolume 5000)
+    if %Action% == "volUp" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd changesysvolume 5000)
 
-if %Action% == "volDown" (
-curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
-nircmd changesysvolume -5000)
+    if %Action% == "volDown" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd changesysvolume -5000)
+REM *ESSAS AÇÕES DE MUSICA AINDA NÃO FORAM IMPLEMENTADAS NO APP*
+
+    if %Action% == "nextMusic" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd sendkey 0xB0 press)
+
+    if %Action% == "prevMusic" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd sendkey 0xB1 press)
+    
+    if %Action% == "startPause" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd sendkey 0xB3 press)
+
+REM *ESSAS AÇÕES DE MUSICA AINDA NÃO FORAM IMPLEMENTADAS NO APP*
+    if %Action% == "muteUnmute" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd mutesysvolume 2)
+
+    if %Action% == "turnOffScreen" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd monitor off)
+
+    if %Action% == "turnOnScreen" (
+    curl -X PUT -d "{\"action\": \"%Action%\", \"executed\": \"true\"}" https://teste-1135e.firebaseio.com/data.json
+    nircmd monitor on)
 
 )
 
---
+++ start.vbs
@@ -0,0 +1,3 @@
+Dim Shell
+Set Shell = CreateObject("WScript.Shell")
+Shell.Run "PcControl.bat", 0
